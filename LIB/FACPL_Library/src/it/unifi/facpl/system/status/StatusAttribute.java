3f87390c2d05a1364c5a2000d939c3a32ff827e8
iskorini <iskorini@gmail.com>
2016-02-01 10:35:16 +0100

    Implementazione iniziale degli attributi

--- LIB/FACPL_Library/src/it/unifi/facpl/system/status/StatusAttribute.java
+++ LIB/FACPL_Library/src/it/unifi/facpl/system/status/StatusAttribute.java
@@ -3,12 +3,48 @@
  */
 package it.unifi.facpl.system.status;
 
+import it.unifi.facpl.lib.enums.FacplStatusType;
+
 /**
- * @author 
+ * class for status attribute	
  *
  */
 public class StatusAttribute {
 
+	private String id;
+	private FacplStatusType type;
+	/**
+	 * @param id
+	 * @param type
+	 */
+	public StatusAttribute(String id, FacplStatusType type) {
+		this.id = id;
+		this.type = type;
+	}
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * @return the type
+	 */
+	public FacplStatusType getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(FacplStatusType type) {
+		this.type = type;
+	}
 	
 	
 	
