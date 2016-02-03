e2e5737d6ead792119b3ba80136ca03403b7ffe3
iskorini <iskorini@gmail.com>
2016-02-03 16:21:08 +0100

    corretto errore

--- LIB/FACPL_Library/src/it/unifi/facpl/lib/function/comparison/AbstractComparison.java
+++ LIB/FACPL_Library/src/it/unifi/facpl/lib/function/comparison/AbstractComparison.java
@@ -24,15 +24,10 @@ public abstract class AbstractComparison  implements IComparisonFunction {
 			Object o1, o2;
 			o1 = args.get(0) instanceof StatusAttribute? ((StatusAttribute)args.get(0)).getValue() : args.get(0);
 			o2 = args.get(1) instanceof StatusAttribute? ((StatusAttribute)args.get(1)).getValue() : args.get(1);
-			if (!(o1 instanceof StatusAttribute) ^ !(o2 instanceof StatusAttribute) ) {
-				throw new Exception("One is attribute and other no");
-				/*
-				 * magari fare mix dei due casi ovvero:
-				 * args[0]: StatusAttribute
-				 * args[1]: valore
-				 * si estrae il valore da args[0] e si confronta con args[1] o viceversa
-				 */
-			}
+			/*
+			 * NEL CASO IL VALORE SIA RICAVATO DA UNO STATUS ATTRIBUTE E' UNA STRINGA, QUINDI ANDREBBE FATTO UN CONTROLLO 
+			 * ED EVENTUALMENTE UN CAST
+			 */
 			IComparisonEvaluator evaluator = ComparisonEvaluatorFactory.getInstance().getEvaluator(o1);
 			return op(evaluator, o1, o2);
 			
