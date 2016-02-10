c16285bd95be31a15c94563d4bb89f2f67ef5447
Andrea Margheri <margheri.andrea@gmail.com>
2016-02-10 23:36:04 +0100

    Fix Copyright

--- EXAMPLES/FACPL-WebTutorial/src/eHealthWeb/ContextStub_eHealth.java
+++ EXAMPLES/FACPL-WebTutorial/src/eHealthWeb/ContextStub_eHealth.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Andrea Margheri
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andrea Margheri
+ *******************************************************************************/
 package eHealthWeb;
 
 import it.unifi.facpl.lib.interfaces.IContextStub;
